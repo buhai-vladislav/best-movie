3f7bd78b8db2511f7571ac920d0da42042716f4e
Vladyslav Buhai <vladbugaj30@gmail.com>
2023-07-16 13:55:38 +0300

    remove unused packages

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,14 @@ import App from './App'
 import { NextUIProvider } from '@nextui-org/react'
 import { Provider } from 'react-redux'
 import { store } from './store'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="938320411765-b6a1j5h9p34im2bqep00j1ndsj8hah1n.apps.googleusercontent.com">
-      <Provider store={store}>
-        <NextUIProvider>
-          <App />
-        </NextUIProvider>
-      </Provider>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </Provider>
   </React.StrictMode>,
 )
